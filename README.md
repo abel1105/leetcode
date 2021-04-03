73b4f254258c88f14019d1f0abe2c0cd266e9cf0
Abel Lee <abel.lee@linecorp.com>
2021-04-03 16:36:41 +0800

    Add 485 max consecutive ones

---
+++ README.md
@@ -0,0 +1,9 @@
+#LeetCode
+
+##LeetCode Algorithm
+
+### Array
+
+|#	|Title|	Solution| Difficulty|
+|---|---|---|---|
+|485|[Max Consecutive Ones](https://leetcode.com/problems/max-consecutive-ones)|[Python](./algorithms/485_Max_Consecutive_Ones.py)| Easy |
---
+++ algorithms/485_Max_Consecutive_Ones.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max = 0
+        current = 0
+
+        for num in nums:
+            if num == 1:
+                current += 1
+            else:
+                if current > max:
+                    max = current
+                current = 0
+        # if the last time current is the max then you have to return the current
+        return current if current > max else max
+
+
+# Time complexity: O(n)
+# Runtime: 280 ms
+# Memory Usage: 13.7 MB
+
+
+solution0 = Solution()
+print(solution0.findMaxConsecutiveOnes([0]) == 0)
+solution1 = Solution()
+print(solution1.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1, 0, 0, 0]) == 2)
+solution2 = Solution()
+print(solution2.findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1, 1, 1, 1]) == 4)
