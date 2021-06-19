401b9b1220f844f3ea9dd693913d28f5ed7ba730
Abel Lee <abel.lee@linecorp.com>
2021-06-19 17:03:27 +0800

    Add 21 Merge Two Sorted Lists

---
+++ algorithms/21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,59 @@
+# Input: l1 = [1,2,4], l2 = [1,3,4]
+# Output: [1,1,2,3,4,4]
+from typing import List
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        front = c1 = ListNode(0, l1)
+        c2 = ListNode(0, l2)
+
+        while c1.next and c2.next:
+            if c1.next.val <= c2.next.val:
+                c1 = c1.next
+            else:
+                temp = c2.next
+                c2.next = c2.next.next
+                temp.next = c1.next
+                c1.next = temp
+
+        if c2.next:
+            c1.next = c2.next
+
+        return front.next
+
+
+# Time complexity: O(N)
+# Space Complexity: O(1)
+# Runtime: 28ms (98.21%)
+# Memory Usage: 14.2MB (83.69%)
+
+def printVal(node: ListNode) -> str:
+    other = ',' + printVal(node.next) if node.next else ''
+    return str(node.val) + other
+
+
+solution0 = Solution()
+print(solution0.mergeTwoLists(None, None) == None)
+
+i0 = ListNode(1)
+i1 = ListNode(2)
+i2 = ListNode(3)
+i0.next = i1
+i1.next = i2
+
+j0 = ListNode(1)
+j1 = ListNode(2)
+j2 = ListNode(3)
+j0.next = j1
+j1.next = j2
+
+solution1 = Solution()
+print(printVal(solution1.mergeTwoLists(i0, j0)) == '1,1,2,2,3,3')
